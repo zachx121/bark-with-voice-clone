4cee3e4f89c3b80ac5c00a0c190c407542ad4fa6
lukaiyuan <lukaiyuan@didiglobal.com>
2023-05-24 01:44:01 +0800

    long sentence

---
+++ tmp.py
@@ -0,0 +1,14 @@
+from bark.generation import preload_models
+
+preload_models(
+    text_use_gpu=True,
+    text_use_small=False,
+    coarse_use_gpu=True,
+    coarse_use_small=False,
+    fine_use_gpu=True,
+    fine_use_small=False,
+    codec_use_gpu=True,
+    force_reload=False,
+    path="models"
+)
+
